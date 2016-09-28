b8baec0141e9d970f58c3138e9ea430c06e015d2
EMilborn <emilborn@stuy.edu>
2016-09-28 12:32:57 -0400

    updated with new thingies

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/login")
 def getForm():
-    return render_template('form.html', name01 = "TEXT GOES HERE")
+    return render_template('form.html',
+                           name01 = "TEXT GOES HERE",
+                           heading01 = "HEADING")
+
+@app.route("/authenticate/")
+def auth():
+    print request.headers
+    return "OK"
 
 if __name__ == "__main__":
     #enable debugging, auto-restarting of server when this file is modified
